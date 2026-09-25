5d4afc95f9efab47b5a0e03b6e7966453ef7d07a
Yazaaaaaa <yann.malzieu@eleve.ensai.fr>
2026-09-25 07:51:20 +0000

    modif data

--- backend/data/init_db.sql
+++ backend/data/init_db.sql
@@ -26,20 +26,20 @@ CREATE TABLE NEOW.neo (
     absolute_magnitude  FLOAT,
     is_hazardous        BOOLEAN,
     is_custom           BOOLEAN,
-    created_by_user_id  INT REFERENCES NEOW.users(id_user) -- Correction ici
+    created_by_user_id  INT REFERENCES NEOW.users(id_user)
 );
 
 CREATE TABLE NEOW.login_history (
     id_login    SERIAL PRIMARY KEY,
-    id_user     INT REFERENCES NEOW.users(id_user),    -- Correction ici
+    id_user     INT REFERENCES NEOW.users(id_user),
     login_at    TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
     ip_address  VARCHAR(20)
 );
 
 CREATE TABLE NEOW.alert (
     id_alert          SERIAL PRIMARY KEY,
-    id_user           INT REFERENCES NEOW.users(id_user), -- Correction ici
-    id_neo            INT REFERENCES NEOW.neo(id_neo),    -- Correction ici
+    id_user           INT REFERENCES NEOW.users(id_user),
+    id_neo            INT REFERENCES NEOW.neo(id_neo),
     min_size_m        FLOAT,
     max_distance_km   FLOAT,
     is_active         BOOLEAN,
@@ -48,15 +48,15 @@ CREATE TABLE NEOW.alert (
 
 CREATE TABLE NEOW.notification (
     id_notification SERIAL PRIMARY KEY,
-    id_user         INT REFERENCES NEOW.users(id_user),   -- Correction ici
-    id_alert        INT REFERENCES NEOW.alert(id_alert),  -- Correction ici
+    id_user         INT REFERENCES NEOW.users(id_user),
+    id_alert        INT REFERENCES NEOW.alert(id_alert),
     message         TEXT,
     sent_at         TIMESTAMP DEFAULT CURRENT_TIMESTAMP
 );
 
 CREATE TABLE NEOW.close_approach (
     id_close_approach     SERIAL PRIMARY KEY,
-    id_neo                INT REFERENCES NEOW.neo(id_neo), -- Correction ici
+    id_neo                INT REFERENCES NEOW.neo(id_neo),
     approach_date         DATE,
     miss_distance_km      FLOAT,
     relative_velocity_kmh FLOAT
@@ -64,14 +64,14 @@ CREATE TABLE NEOW.close_approach (
 
 CREATE TABLE NEOW.favorite (
     id_favorite SERIAL PRIMARY KEY,
-    id_user     INT REFERENCES NEOW.users(id_user),   -- Correction ici
-    id_neo      INT REFERENCES NEOW.neo(id_neo),      -- Correction ici
+    id_user     INT REFERENCES NEOW.users(id_user),
+    id_neo      INT REFERENCES NEOW.neo(id_neo),
     added_at    TIMESTAMP DEFAULT CURRENT_TIMESTAMP
 );
 
 CREATE TABLE NEOW.distance_history (
     id_distance_history SERIAL PRIMARY KEY,
-    id_favorite         INT REFERENCES NEOW.favorite(id_favorite), -- Correction ici
+    id_favorite         INT REFERENCES NEOW.favorite(id_favorite),
     recorded_at         DATE,
     distance_km         FLOAT
 );
